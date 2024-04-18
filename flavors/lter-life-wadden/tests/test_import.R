217b42b82a5630d8e6d7094404da3161776fc316
Gabriel Pelouze <gabriel@pelouze.net>
2024-04-18 17:55:11 +0200

    fix test

--- flavors/lter-life-wadden/tests/test_import.R
+++ flavors/lter-life-wadden/tests/test_import.R
@@ -1,9 +1,9 @@
 library(optparse)
 library(jsonlite)
 
-library(fme)
-library(desolve)
-library(plot3d)
+library(FME)
+library(deSolve)
+library(plot3D)
 library(sf)
 library(ncdf4)
 library(tidyverse)
