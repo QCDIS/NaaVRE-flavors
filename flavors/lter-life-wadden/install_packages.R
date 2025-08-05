828dd470a0da16cfa70332fe441bf90d668e6dcb
Gabriel Pelouze <gabriel@pelouze.net>
2025-08-05 09:58:03 +0200

    fix lter-life-wadden build

---
+++ flavors/lter-life-wadden/install_packages.R
@@ -0,0 +1,8 @@
+devtools::install_github('krietsch/tools4watlas@952cb1038c862075bb2df2dfc061e2df87b04340', dependencies = FALSE)
+library('tools4watlas')
+
+devtools::install_github('allertbijleveld/SIBES@24d3f06418a8aca77db4e7fbef66c45dc55873f8', dependencies = FALSE)
+library('SIBES')
+
+devtools::install_github('TempSED/TempSED@79ecf0e2f9473c3e5e35cf53bc87d15389f2e68c', depend = TRUE)
+require('TempSED')
